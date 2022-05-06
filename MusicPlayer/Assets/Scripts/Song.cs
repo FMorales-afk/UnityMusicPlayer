2aa59a05d7edd26f64cc54a1cf37de2cfc29f97b
Haydrill <saldua.h@gmail.com>
2022-05-06 13:40:28 -0700

    asdhjkl

--- MusicPlayer/Assets/Scripts/Song.cs
+++ MusicPlayer/Assets/Scripts/Song.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using TMPro;
 
 public class Song : MonoBehaviour
 {
@@ -12,6 +12,26 @@ public class Song : MonoBehaviour
     public string album;
     public TimeSpan songLength;
 
+    public AudioClip song;
+
+    public AudioSource player;
+
+    public TextMeshProUGUI numText;
+    public TextMeshProUGUI songText;
+    public TextMeshProUGUI artistText;
+    public TextMeshProUGUI albumText;
+    public TextMeshProUGUI lengthText;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<AudioSource>();
+        numText.text = num.ToString();
+        songText.text = title;
+        artistText.text = artist;
+        albumText.text = album;
+        lengthText.text = songLength.ToString();
+    }
+
     public Song()
     {
         num = 0;
@@ -19,5 +39,12 @@ public class Song : MonoBehaviour
         artist = "artist";
         album = "album";
         songLength = new TimeSpan(0, 0, 0);
+    }
+
+    public void OnClickPlay()
+    {
+        player.Stop();
+        player.clip = song;
+        player.Play();
     }
 }
